85d2e561ba27332be70954f45bf2c8b0937826a0
Judd Baconawa <75963954+JuddBaconawa@users.noreply.github.com>
2025-02-06 22:24:22 -0500

    transfered the files correctly
    learned how to safe a file in my local machine the right way

---
+++ java-CurrencyConverter/src/App.java
@@ -0,0 +1,30 @@
+//packages
+
+//imports
+import java.util.Scanner;
+
+//main class - App
+public class App {
+
+    // main class that runs the app
+    public static void main(String[] args) throws Exception {
+
+        // scanner initialized
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(
+                "Welcome! Welcome to our money converter. Enter your currency first, followed by what currency you want to convert the value to.");
+        pressAnyKeyToContinue();
+        System.out.println("Enter 1-4 to identify your currency:");
+        
+        
+            }
+        
+            public static void pressAnyKeyToContinue() {
+        System.out.println("Press enter to Continue.....");
+        Scanner s = new Scanner(System.in);
+        s.nextLine();
+    }
+}
+
+
---
+++ java-CurrencyConverter/src/Calculations.java
@@ -0,0 +1,14 @@
+//packages
+
+
+//imports
+
+//class - calculations
+public class Calculations {
+
+	public static double usDollars() {
+		double usDollars = 1;
+		return firstCurrency * usDollars;
+	}
+	
+}
